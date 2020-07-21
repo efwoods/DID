ffe94046b23b40001d501fb7f9fe298d16218452
Evan Woods <efwoods@g.clemson.edu>
2020-07-21 19:51:36 -0400

    init

---
+++ src/main/java/com/boeing/DID/DidApplication.java
@@ -0,0 +1,13 @@
+package com.boeing.DID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boeing/DID/DidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boeing.DID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
